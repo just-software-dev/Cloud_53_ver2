8409a83efdbe4c1936dd19452ec52d4687dd31c8
Andrey <golubenko98@gmail.com>
2020-08-09 02:08:48 +0300

    Initial Commit

---
+++ Cloud_53_ver2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cloud_53_ver2
+//
+//  Created by Андрей on 09.08.2020.
+//  Copyright © 2020 oak. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cloud_53_ver2Tests/Cloud_53_ver2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Cloud_53_ver2Tests.swift
+//  Cloud_53_ver2Tests
+//
+//  Created by Андрей on 09.08.2020.
+//  Copyright © 2020 oak. All rights reserved.
+//
+
+import XCTest
+@testable import Cloud_53_ver2
+
+class Cloud_53_ver2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
